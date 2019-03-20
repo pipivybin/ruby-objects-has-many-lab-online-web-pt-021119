b712efc8152489472bab26dfe736d4b2070db77e
Ming Duan <11203duan@gmail.com>
2019-03-20 02:13:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,27 @@
 class Artist
 
-attr_accessor :name
+attr_accessor :name, :songs
 
 def initialize(name)
   @name = name
 end
 
 def songs
-  
+
 end
+
+def add_song(song)
+  song = Song.new
+  song.name = songname
+  song.artist = self
+end
+
+def add_song_by_name(songname)
+  song = Song.new
+  song.name = songname
+  song.artist = self
+end
+
+
 
 end
